4852ec59216759cd9ae215c405d5aa7d68822524
unknown <may4028@IMM-9.main.ad.rit.edu>
2015-10-07 11:33:09 -0400

    Game Pushed before was made with 3D instead of 2D. Game has now been swapped to 2D.

---
+++ 2DJohn/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 10f;
+	// Update is called once per frame
+	void Update () {
+
+		if (Network.isServer) {
+			PlayerInput();
+		}
+
+	}
+
+	private void PlayerInput() {
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+	}
+}
