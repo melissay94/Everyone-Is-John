bffdd750b4bb5ea73fc843f145dd0685328619fa
solanin <sjc5283@g.rit.edu>
2015-12-16 10:35:31 -0500

    Basic Interactions Set Up

---
+++ FinalJohn/Assets/GameScripts/Scripts/InteractableObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableObject : MonoBehaviour {
+	
+	public int type;
+	string desc;
+	Action[] actions;
+
+	// Use this for initialization
+	void Start () {
+
+		switch (type) {
+		
+		case 0:
+			desc = "Tree";
+			actions = new Action[6];
+			for (int i = 0; i < actions.Length; i++) {
+				actions[i] = new Action(i);
+			}
+			break;
+		case 1:
+			desc = "NPC";
+			actions = new Action[7];
+			for (int i = 0; i < actions.Length; i++) {
+				actions[i] = new Action(6+i);
+			}
+			break;
+		case 2:
+			desc = "House";
+			actions = new Action[11];
+			for (int i = 0; i < actions.Length; i++) {
+				actions[i] = new Action(13+i);
+			}
+			break;
+		}
+	}
+}
